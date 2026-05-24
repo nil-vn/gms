beecb546165168e2bd797d5e1f8e3de642c99bd4
tinhtn <trannt.90@gmail.com>
2026-05-24 23:10:34 +0900

    i18n

--- babel.bat
+++ babel.bat
@@ -1,5 +1,5 @@
 #pybabel init -i lang/messages.pot -d lang/translations -l ja
 
-pybabel extract -F babel.cfg -k lazy_gettext -o lang/messages.pot .
+pybabel extract -F babel.cfg -k lazy_gettext -o lang/messages.pot --ignore-dirs="env .env venv genv" .
 pybabel update -i lang/messages.pot -d lang/translations
 pybabel compile -d lang/translations
